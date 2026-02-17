e314f8623499869a581664ea7c8319112c559593
Osiris Mariano <oslimacontato@gmail.com>
2026-02-17 07:27:23 -0300

    conceito sobre operadores aritimeticos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,61 @@
 ﻿using ExemploFundamentos.Models;
-DateTime dataAtual = DateTime.Now.AddDays(5);
+string a = "10-";
+int b = 0;
+int.TryParse(a, out b);
 
-Console.WriteLine("Data atual: " + dataAtual.ToString("dd/MM/yyyy"));
+Console.WriteLine("Valor de b: " + b);
+
+
+
+
+// double a = 4 / (2 + 2);
+// Console.WriteLine(a);
+
+
+
+
+// int a = 10;
+// double b = a;
+// long a = long.MaxValue;
+// int b = Convert.ToInt32(a);
+
+// int a = int.MaxValue;
+// long b = a;
+
+// Console.WriteLine("Valor de a: " + b);
+
+
+
+
+// int inteiro = 10;
+// string a = inteiro.ToString();
+
+// Console.WriteLine("Valor de a: " + a);
+
+
+
+
+
+
+
+// int a = Convert.ToInt32("10");
+// a = 10;
+// Console.WriteLine("Valor de a: " + a);
+
+
+
+// int a = 10;
+// int b = 20;
+// int c = a + b;
+//c = c + 5;
+// c += 5;
+// c -= 5;
+// Console.WriteLine(c);
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+
+// Console.WriteLine("Data atual: " + dataAtual.ToString("dd/MM/yyyy"));
 
 // string apresentacao = "Olá, seja bem vindo ao curso de C# Fundamentos!";
 
